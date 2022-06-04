09b7562215688f54ab29fe5165cd427e6d23c13a
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-06-04 10:05:02 -0400

    wip

--- lib/index.ts
+++ lib/index.ts
@@ -8,6 +8,12 @@ export function getHeader(inBuff: Buffer) {
     // Verify that the bitstream is at least two bytes long
     if (inBuff.length < 2) { throw new Error('getheader(): buffer too small')}
 
+    // The first byte of the header is either 0 or 1, and determines the way
+    // in which literal bytes are represented in the bitstream (see below).
     const litteralMode = inBuff[0]
+
+    // The second byte of the header is equal to 4, 5, or 6, and determines
+    // the size of the sliding-window dictionary: 4, 5, and 6 indicate a 1K,
+    // 2K and 4K dictionary respectively.    
     const dictionarySize = inBuff[1]
 }
